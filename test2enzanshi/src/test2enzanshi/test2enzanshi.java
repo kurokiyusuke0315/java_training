eebbc7f8d83351d8ef4b32d34db87ad3aef8af77
kurok <kurok@yusuke>
2024-03-02 17:23:39 +0900

    tese2enzanshi

---
+++ test2enzanshi/src/test2enzanshi/test2enzanshi.java
@@ -0,0 +1,33 @@
+//実力確認問題　2．演算子 kuroki yusuke
+
+package test2enzanshi;
+
+class test2enznshi
+{
+	public static void main(String[] args)
+	{
+		//2-1
+		System.out.println("Heloo World!");
+		
+		//2-2
+		String a,b;
+		a = "Hello ";
+		b = "World!";
+		System.out.println(a+b);
+		
+		//2-3
+		int x,y,z;
+		x = 3;
+		y = 6;
+		z = x;
+		x = y;
+		y = z;
+		System.out.println(x);
+		System.out.println(y);
+		
+		//2-4
+		x = 7;
+		System.out.println(x*3);
+		System.out.println(x/2);		
+	}
+}
