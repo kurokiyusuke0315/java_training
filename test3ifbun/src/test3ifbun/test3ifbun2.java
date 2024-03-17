a0492710546527860cce794ef2a5be78b09963bd
kurok <kurok@fumiyo>
2024-03-17 19:33:11 +0900

    test3ifbun

--- test3ifbun/src/test3ifbun/test3ifbun2.java
+++ test3ifbun/src/test3ifbun/test3ifbun2.java
@@ -16,7 +16,7 @@ class test3ifbun2
 	public static void main(String[] args)throws IOException
 	{		
 		BufferedReader br =
-		 new BufferedReader(new InputStreamReader(System.in));
+		new BufferedReader(new InputStreamReader(System.in));
 
 		System.out.println("xの値を入力してください。");
 		String str1 = br.readLine();
@@ -31,9 +31,9 @@ class test3ifbun2
 		if(x == y && (x + y) <= -1) {
 			System.out.println("xとyは等しく、かつ、負の数である。");
 		}
-		else if(x < y && (x + y) % 2 == 0) {
+		if(x < y && x % 2 == 0 && y % 2 == 0) {
 			System.out.println("xはyより小さく、かつ、x と y は共に偶数である。");
-			}
+		}
 		if(x < y || x % 2 == 0) {
 			System.out.println("x は yより小さい、または、x は偶数である。");
 		}
